f6c346c0c98ed90c22512ba787ae23a1d54d104d
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-07 17:44:46 +0330

    [fix] add styles and make changes
    Add style for main header for logo and navbar.
    Also add NextLink to logo component and make that component to accept className props.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,13 @@
 import logoImage from "@/assets/logo.png";
+import Link from "next/link";
 
-const Logo: React.FC = () => {
-  return <img src={logoImage.src} alt="A plate with food on it." />;
+const Logo: React.FC<{ className: string }> = ({ className }) => {
+  return (
+    <Link href="/" className={className}>
+      <img src={logoImage.src} alt="A plate with food on it." />
+      NextLevel Food
+    </Link>
+  );
 };
 
 export default Logo;
--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import Logo from "./logo";
+import styles from "./main-header.module.css";
 
 const MainHeader: React.FC = () => {
   return (
-    <header>
-      <Link href="/">
-        <Logo />
-      </Link>
-      <nav>
+    <header className={styles.header}>
+      <Logo className={styles.logo} />
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
