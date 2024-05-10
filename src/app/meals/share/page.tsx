08a5b0a1155343867b9b26a86ce374a6ff347d6a
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-10 17:29:11 +0330

    [feat] create shareMeal function as serverAction

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -2,6 +2,21 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData: FormData) {
+    "use server";
+
+    const meal = {
+      creator: formData.get("name"),
+      title: formData.get("title"),
+      summery: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +26,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +54,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="image" />
+          <ImagePicker label="your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
