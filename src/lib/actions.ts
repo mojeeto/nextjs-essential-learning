28f2af42e96d7e9b3322954834713fe4d7a481f3
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-10 19:30:20 +0330

    [feat] Save meal logic
    Create object of formdata then use saveMeal function to save it to db.
    Image is file so then i write it to public/image then save the path of it in db.

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,14 +1,20 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+
 export async function shareMeal(formData: FormData) {
   const meal = {
-    creator: formData.get("name"),
-    title: formData.get("title"),
-    summery: formData.get("summary"),
-    instructions: formData.get("instructions"),
-    image: formData.get("image"),
-    creator_email: formData.get("email"),
+    creator: formData.get("name") as string,
+    title: formData.get("title") as string,
+    summary: formData.get("summary") as string,
+    instructions: formData.get("instructions") as string,
+    image: formData.get("image") as File,
+    creator_email: formData.get("email") as string,
+    slug: "",
   };
+  // fix the error
+  await saveMeal(meal);
 
-  console.log(meal);
+  return redirect("/meals");
 }
