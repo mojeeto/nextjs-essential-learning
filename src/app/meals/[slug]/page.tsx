6d2f938eecf4d1b02ab00e28ffb4940e1f6e380e
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-10 15:38:39 +0330

    [feat] make meals/[slug] page better

--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -1,12 +1,37 @@
-export default function MealsSlugPage({
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { MealItemType } from "@/components/meals/meals-grid";
+
+export default function MealDeatilsPage({
   params,
 }: {
   params: { slug: string };
 }) {
+  const meal = getMeal(params.slug) as MealItemType;
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <main className="text-white">
-      <h3>This is slug page</h3>
-      <span>Slug: {params.slug}</span>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
