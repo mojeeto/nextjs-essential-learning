9b8ee68bd8834d778678a3da75baebb2671054e8
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-11 11:51:56 +0330

    [feat] custom error page for share meals

---
+++ src/app/meals/share/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Invalid Inputs!</h1>
+      <p>Faild to save meals.</p>
+    </main>
+  );
+}
