455e59f1737a39b5bce2dbb270cb415e48b41a0f
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-11 11:52:08 +0330

    [feat] backend validation for ServerAction

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function IsInputTextValid(text: string) {
+  return !text || text.length !== 0;
+}
+
 export async function shareMeal(formData: FormData) {
   const meal = {
     creator: formData.get("name") as string,
@@ -13,7 +17,20 @@ export async function shareMeal(formData: FormData) {
     creator_email: formData.get("email") as string,
     slug: "",
   };
-  // fix the error
+
+  if (
+    IsInputTextValid(meal.title) ||
+    IsInputTextValid(meal.creator) ||
+    IsInputTextValid(meal.summary) ||
+    IsInputTextValid(meal.instructions) ||
+    IsInputTextValid(meal.creator_email) ||
+    meal.creator_email.includes("@") ||
+    !image ||
+    image.size !== 0
+  ) {
+    throw new Error("Input is invalid!");
+  }
+
   await saveMeal(meal);
 
   return redirect("/meals");
